a1f6536ffe9aa37b2adab35fac012b97f11c1b9e
Van Nguyen <thegoleffect@gmail.com>
2012-03-07 09:33:46 -0800

    variable name fixes

--- test/utils.types.js
+++ test/utils.types.js
@@ -9,10 +9,14 @@ describe("Types", function(){
       done();
     })
     
-    
     it("should have a .registry", function(done){
       should.exist(Types.registry);
       // console.log(Types.registry);
+      done();
+    })
+    
+    it("should have some presets loaded", function(done){
+      (Object.keys(Types.registry).length).should.be.above(1)
       done();
     })
   })
@@ -94,10 +98,58 @@ describe("Types", function(){
     })
   })
   
-  describe("usage", function(){
-    it("should have some presets loaded", function(done){
-      (Object.keys(Types.registry).length).should.be.above(1)
+  describe("#validateCollection", function(){
+    var req, conf, User;
+    // simulate real arguments
+    
+    
+    beforeEach(function(done){
+      User = {
+        get: function(req, res){}
+      }
+      req = {
+        params: {
+          id: "1"
+        },
+        query: {
+          access_token: "SFFi4o/AQXKKTf60Ejke8C/2Lp0m6EF5nvDGfy9tnmQ="
+        }
+      }
+      conf = {
+        method: "GET",
+        path: "/user/:id",
+        handler: User.get,
+        query: {
+          id: {
+            type: "String"
+          },
+          access_token: {
+            type: "String"
+          }
+        }
+      }
       done();
     })
+    
+    describe("(with just parameters)", function(){})
+    
+    describe("(with variable arguments in route/path)", function(){
+      it("should validate String route arg as String", function(done){
+        Types.validateCollection(req, conf, function(err){
+          should.not.exist(err);
+          done();
+        })
+      })
+      
+      it("should not validate String route as Email", function(done){
+        conf.query.id.type = "Email";
+        Types.validateCollection(req, conf, function(err){
+          should.exist(err);
+          done();
+        })
+      })
+    }) // TODO:
+    
+    describe("(with variable arguments in parameters", function(){}) // TODO:
   })
 })
